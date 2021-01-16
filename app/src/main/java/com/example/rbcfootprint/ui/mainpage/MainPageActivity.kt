1e2b63c992b8b1c2c17809c46011f6ee0a831835
Yukie Man <yukie.man97@gmail.com>
2021-01-16 06:36:35 -0800

    First commit

---
+++ app/src/main/java/com/example/rbcfootprint/ui/mainpage/MainPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rbcfootprint.ui.mainpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rbcfootprint.R
+
+class AnotherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
